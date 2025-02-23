8af893d04d23ba926d10fb06f5bdaa3e14e84836
Dario Varriale <dario.varriale@atobit.it>
2025-02-23 15:33:37 +0100

    Add automatic music startup + fixes on user input

--- arena-fortune-wheel/flutter_bootstrap.js
+++ arena-fortune-wheel/flutter_bootstrap.js
@@ -9,8 +9,47 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac40d","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
+const loading = document.createElement('div');
+
+loading.style.width = '100svw';
+loading.style.height = '100svh';
+loading.style.margin = '0';
+loading.style.padding = '0';
+loading.style.display = 'flex';
+loading.style.justifyContent = 'center';
+loading.style.alignItems = 'center';
+
+loading.innerHTML = `
+    <style>
+        .loader {
+            width: 8rem;
+            height: 8rem;
+            animation: spin 2s ease-out infinite;
+        }
+        .loader img {
+            width: 100%;
+            height: 100%;
+        }
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+    </style>
+    <div class="loader">
+        <img src="icons/Icon-192.png" alt="loading-logo"/>
+    </div>
+`;
+document.body.appendChild(loading);
+
+_flutter.buildConfig.builds[0].mainJsPath += "?v=" + serviceWorkerVersion;
+
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3895065382"
-  }
+    serviceWorkerSettings: {
+        serviceWorkerVersion: serviceWorkerVersion
+    },
+    onEntrypointLoaded: async function(engineInitializer) {
+        const appRunner = await engineInitializer.initializeEngine();
+        await appRunner.runApp();
+        document.body.removeChild(loading);
+    },
 });
